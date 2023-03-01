241130e45a6ecee53e61fd603c16e51af1791eea
Fergus2299 <fergusjcassidy@gmail.com>
2023-03-01 00:44:21 +0000

    cors permissions for database access

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const app = express();
 app.use(express.json());
 const db = require('./models');
 
+const cors = require("cors");
 
+// this helps with permissions, * is wild meaning any origin can affect the api
+app.use(cors({
+    origin:'*'
+}));
 // routes
 const userRouter = require('./routes/Users');
 app.use("/users", userRouter);
