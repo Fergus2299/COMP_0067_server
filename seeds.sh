20d5f300b568cfc6b2c4a904eec90b8cd81bb487
Fergus2299 <fergusjcassidy@gmail.com>
2023-03-26 20:52:08 +0100

    small test change

--- seeds.sh
+++ seeds.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo Seed files running ...
-sequelize db:seed --seed 20230316232610-programs-seeder.js
-sequelize db:seed --seed 20230319185759-internalusers-seeder.js
-sequelize db:seed --seed 20230326181536-companies-seeder.js
-sequelize db:seed --seed 20230325224908-externalusers-seeder.js
+node_modules/.bin/sequelize db:seed --seed 20230316232610-programs-seeder.js
+node_modules/.bin/sequelize db:seed --seed 20230319185759-internalusers-seeder.js
+node_modules/.bin/sequelize db:seed --seed 20230326181536-companies-seeder.js
+node_modules/.bin/sequelize db:seed --seed 20230325224908-externalusers-seeder.js
 echo Seed Successful, enjoy the rest of your day Mr Cassidy!
-echo ;)
